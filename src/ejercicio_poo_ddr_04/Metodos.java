6756649eee0b4062a346a57ab952ba0f654c3282
Cardj <carlos.dj.flores@hotmail.com>
2023-08-19 10:15:12 -0600

    Se crea el proyecto Cine

---
+++ src/ejercicio_poo_ddr_04/Metodos.java
@@ -0,0 +1,13 @@
+
+package ejercicio_poo_ddr_04;
+
+public class Metodos {
+
+    public static final String nombres[] = {"Fernando", "Laura", "Pepe", "Eufrasio"};
+
+    public static int generaNumeroEnteroAleatorio(int minimo, int maximo) {
+        int num = (int) (Math.random() * (minimo - (maximo + 1)) + (maximo + 1));
+        return num;
+    }
+
+}
